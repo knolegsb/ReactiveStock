d89be6303e729298adeabd0433f1926585403689
Sean <seon2you@hotmail.com>
2016-08-13 09:36:10 -0700

    actor system references

---
+++ ReactiveStock/ActorModel/ActorSystemReferences.cs
@@ -0,0 +1,29 @@
+﻿using Akka.Actor;
+using Akka.DI.Core;
+using Akka.DI.Ninject;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveStock.ActorModel
+{
+    static class ActorSystemReferences
+    {
+        public static ActorSystem ActorSystem { get; private set; }
+
+        static ActorSystemReferences()
+        {
+            CreateActorSystem();
+        }
+
+        private static void CreateActorSystem()
+        {
+            ActorSystem = ActorSystem.Create("ReactiveStockActorSystem");
+            var container = new StandardKernel();
+            IDependencyResolver resolver = new NinjectDependencyResolver(container, ActorSystem);
+        }
+    }
+}
