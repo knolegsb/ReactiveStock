05fc26ec87b50339dd87d12ace58132e5fd8ee26
Sean <seon2you@hotmail.com>
2016-08-14 14:55:53 -0700

    stock toggle button actor

---
+++ ReactiveStock/ActorModel/Actors/UI/StockToggleButtonActor.cs
@@ -0,0 +1,49 @@
+﻿using Akka.Actor;
+using ReactiveStock.ActorModel.Messages;
+using ReactiveStock.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveStock.ActorModel.Actors.UI
+{
+    class StockToggleButtonActor : ReceiveActor
+    {
+        private readonly IActorRef _coordinatorActor;
+        private readonly string _stockSymbol;
+        private readonly StockToggleButtonViewModel _viewModel;
+
+        public StockToggleButtonActor(IActorRef coordinatorActor, StockToggleButtonViewModel viewModel, string stockSymbol)
+        {
+            _coordinatorActor = coordinatorActor;
+            _viewModel = viewModel;
+            _stockSymbol = stockSymbol;
+
+            ToggledOff();
+        }
+
+        private void ToggledOff()
+        {
+            Receive<FlipToggleMessage>(
+                mesage =>
+                {
+                    _coordinatorActor.Tell(new WatchStockMessage(_stockSymbol));
+                    _viewModel.UpdateButtonTextToOn();
+                    Become(ToggledOn);
+                });
+        }
+
+        private void ToggledOn()
+        {
+            Receive<FlipToggleMessage>(
+                message =>
+                {
+                    _coordinatorActor.Tell(new UnWatchStockMessage(_stockSymbol));
+                    _viewModel.UpdateButtonTextToOff();
+                    Become(ToggledOff);
+                });
+        }
+    }
+}
---
+++ ReactiveStock/ViewModel/StockToggleButtonViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Akka.Actor;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using ReactiveStock.ActorModel;
+using ReactiveStock.ActorModel.Actors.UI;
+using ReactiveStock.ActorModel.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ReactiveStock.ViewModel
+{
+    public class StockToggleButtonViewModel : ViewModelBase
+    {
+        private string _buttonText;
+        public string StockSymbol { get; set; }
+        public ICommand ToggleCommand { get; set; }
+        public IActorRef StockToggleButtonActorRef { get; private set; }
+        public string ButtonText
+        {
+            get { return _buttonText; }
+            set { Set(() => ButtonText, ref _buttonText, value); }
+        }
+
+        public StockToggleButtonViewModel(IActorRef stockCoordinatorRef, string stockSymbol)
+        {
+            StockSymbol = stockSymbol;
+            StockToggleButtonActorRef = ActorSystemReferences.ActorSystem.ActorOf(Props.Create(() => 
+                                        new StockToggleButtonActor(StockToggleButtonActorRef, this, stockSymbol)));
+
+            ToggleCommand = new RelayCommand(() => StockToggleButtonActorRef.Tell(new FlipToggleMessage()));
+            UpdateButtonTextToOff();
+        }
+
+        public void UpdateButtonTextToOff()
+        {
+            ButtonText = ConstructButtonText(false);
+        }
+
+        public void UpdateButtonTextToOn()
+        {
+            ButtonText = ConstructButtonText(true);
+        }
+
+        private string ConstructButtonText(bool isToggledOn)
+        {
+            return string.Format("{0} {1}", StockSymbol, isToggledOn ? "(0)" : "(off)");
+        }
+    }
+}
